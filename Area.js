644a678c3de85139dd6ad36cfc8489f1c7750bbb
Sirisha Sirisha <141410246+Sirisha-sundara@users.noreply.github.com>
2024-06-26 11:25:14 +0530

    Add files via upload

---
+++ Area.js
@@ -0,0 +1,20 @@
+function calculateArea(width, height){
+    return width * height;
+}
+console.log(calculateArea(5, 6));
+function calculateArea(width, height){
+    return width * height;
+}
+console.log(calculateArea(5, 10));
+function calculateArea(width = 1, height = 1){
+    return width * height;
+}
+console.log(calculateArea());
+var calculateArea = function Area(width = 2, height = 8){
+    return width * height;
+}
+console.log(calculateArea());
+const calculateArea =(width = 8, height = 9) =>{
+    return width * height;
+}
+console.log(calculateArea());

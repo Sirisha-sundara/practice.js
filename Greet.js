57c102e239ee2a705cb88d2cba0d2d4703fed8f1
Sirisha Sirisha <141410246+Sirisha-sundara@users.noreply.github.com>
2024-06-26 20:14:10 +0530

    Update Greet.js

--- Greet.js
+++ Greet.js
@@ -2,20 +2,19 @@ function greet(name = "Guest") {
     return "Hello, " + name + "!";
 }
 
-console.log(greet("Alice"));   // Output: Hello, Alice !
-console.log(greet("Sirisha")); // Output: Hello, Sirisha !
-console.log(greet());          // Output: Hello, Guest !
+console.log(greet("Alice"));   
+console.log(greet("Sirisha")); 
+console.log(greet());          
 
 var greetFunction = function (name = "Alice") {
     return "Hello, " + name + "!";
 }
 
-console.log(greetFunction());       // Output: Hello, Alice !
-console.log(greetFunction("Guest"));// Output: Hello, Guest !
-
+console.log(greetFunction());       
+console.log(greetFunction("Guest"));
 const greetArrowFunction = (name = "Guest") => {
     return "Hello, " + name + "!";
 };
 
-console.log(greetArrowFunction());      // Output: Hello, Guest !
-console.log(greetArrowFunction("Bob")); // Output: Hello, Bob !
+console.log(greetArrowFunction());     
+console.log(greetArrowFunction("Bob")); 
